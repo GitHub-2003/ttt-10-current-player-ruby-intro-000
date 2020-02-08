aeee5969d133d314eab97e68e3ae915f0c1c0c89
MJU <marie.ju2012@outlook.com>
2020-02-08 01:43:30 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,10 @@
 # This method tak(es in an argument of the board array and
 # returns the number of turns that have been played.
 def turn_count(board)
-
+  board.each do |XorO|
+    if XorO != " "
+      count += 1
+    end
   return count
 end
 
