420ab95311458d04b73a233fe2f65f0ef2916240
MJU <marie.ju2012@outlook.com>
2020-02-08 01:44:24 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,3 +10,9 @@ end
 
 def current_player(board)
 end
+
+board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
+
+puts(turn_count(board1))
+puts(turn_count(board2))
