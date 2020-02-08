674a65bce9ec7d1a97fe062a9befc35baa100dad
MJU <marie.ju2012@outlook.com>
2020-02-08 01:53:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,11 @@
 # This method tak(es in an argument of the board array and
 # returns the number of turns that have been played.
 def turn_count(board)
+  primary_colors = ["Red", "Yellow", "Blue"]
+  primary_colors.each do |color| 		# do begins a block
+            # the lines between the do/end are the block's body
+  puts "Primary Color #{color} is #{color.length} letters long."
+
   board.each do |XorO|
     puts "#{XorO}"
   end
