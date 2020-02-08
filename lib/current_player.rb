dc029c0f8c48c02e914361f0f54250118524004c
MJU <marie.ju2012@outlook.com>
2020-02-08 01:37:38 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,9 @@
+# This method tak(es in an argument of the board array and 
+# returns the number of turns that have been played.
+def turn_count(board)
+  
+  return count
+end
+
+def current_player(board)
+end
